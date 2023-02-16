bef39d84e7b28fcd84aa151f681cafd0e500e6a5
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-02-16 14:45:06 +0800

    Installed ExpressJs, connected Postgres backend

--- backend/index.jsx
+++ backend/index.jsx
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const userRouter = require("./routes/users.jsx")
+
+const { Client } = require('pg')
+require('dotenv').config()
+
+const client = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+})
+
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+})
+
+app.use("/users", userRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +30,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+})   
